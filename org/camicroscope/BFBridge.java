16246b75bfc17dc0062cf357967a7133a53cf1c9
CGDogan <126820728+CGDogan@users.noreply.github.com>
2023-08-04 14:55:03 +0100

    Revert "Update BFBridge.java"
    This reverts commit 1a27eca9f08fea2a8b732ad8ee8ee10ece2bc902.

--- org/camicroscope/BFBridge.java
+++ org/camicroscope/BFBridge.java
@@ -768,7 +768,7 @@ public class BFBridge {
         }
     }
 
-    private void saveError(String s) {
+    private static void saveError(String s) {
         byte[] errorBytes = s.getBytes();
         communicationBuffer.rewind().put(errorBytes);
         lastErrorBytes = errorBytes.length;
